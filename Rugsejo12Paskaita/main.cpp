274af0bf6fa3efcbecf06cd741fca610a4e86ccb
Justinas Zailskas <j.zailskas@ad.viko.lt>
2024-09-12 15:46:21 +0300

    Pridejau include string

--- Rugsejo12Paskaita/main.cpp
+++ Rugsejo12Paskaita/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    cout << "Hello, World!" << endl;
+    //pirma uzduotele
+    cout<<"----------1 uzduotis-------"<<endl;
+    string vardas = "Justinas";
+    string pavarde;
+    int amzius;
+
+    pavarde = "Zailskas";
+    amzius = 33;
+
+    cout << "Studento vardas: " <<vardas<<endl;
+    cout << "Studento pavarde: " <<pavarde<<endl;
+
+    //antra uzduotele
+    cout<<"----------2 uzduotis-------"<<endl;
+
     return 0;
 }
