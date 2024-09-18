aaf88bc714555aae804bea5d90f25b3226e8569f
Justinas Zailskas <j.zailskas@ad.viko.lt>
2024-09-18 09:52:11 +0300

    Rugsejo 18 d. teorijos pavyzdziai

---
+++ rugsejo18teorija/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string fruit = "banana";
+    int number;
+    int num1;
+    if (fruit == "banana") {
+        cout<<"you have a banana"<<endl;
+    } else if (fruit == "apple") {
+        cout<<"you have an apple"<<endl;
+    } else {
+        cout<<"You have something else"<<endl;
+    }
+/*cout<<"____________SWITCH dalis________"<<endl;
+    while (true) {
+        cout << "Enter number (1-2): ";
+        cin >> number;
+        switch (number) {
+            case 1:
+                cout <<"You have a banana"<<endl;
+                break;
+            case 2:
+                cout <<"You have an apple" <<endl;
+                break;
+            default:
+                cout <<"You have something else"<<endl;
+        }
+    }*/
+    cout<<"----for ciklas----"<<endl;
+    cout << "Enter a positive integer: "<<endl;
+    cin >> num1;
+
+    int result = 0;
+
+    for(int i = 1; i <= num1; i++) {
+        result = result + i; //result += i;
+        cout<<"Iteration: "<<i<<" sum: "<<result<<endl;
+    }
+
+    cout<<"Sum: "<<result<<endl;
+    return 0;
+}
