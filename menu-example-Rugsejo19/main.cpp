592a63008102f858e164059adb8245339c9e86e7
Justinas Zailskas <j.zailskas@ad.viko.lt>
2024-09-19 11:04:12 +0300

    Panaudoti ciklai 2 ir 3 opcijoje

--- menu-example-Rugsejo19/main.cpp
+++ menu-example-Rugsejo19/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     int choise = 0;
     int number1, number2, sum;
+    int i = 0;
 
     while (choise != 4) {
         cout <<"Paprastas meniu pasirinkimas"<<endl;
@@ -26,9 +27,18 @@ int main() {
             break;
             case 2:
                 cout <<"Jus pasirinkote antra opcija"<<endl;
+                while (i <= 20) {
+                    if ( i % 2 == 1) {
+                        cout <<"Skaitmuo nelyginis "<<i<<endl;
+                    }
+                    i = i +1; //i++;
+                }
             break;
             case 3:
                 cout <<"Jus pasirinkote trecia opcija"<<endl;
+                for (int j = 0; j <= 20; j++ ) {
+                    cout <<"Skaitmuo "<<j<<endl;
+                }
             break;
             case 4:
                 cout <<"Jus isejote is programos"<<endl;
