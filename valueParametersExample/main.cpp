697a0aaa7c8ac1ae981379fe04cf44a8e4a22d50
Justinas Zailskas <j.zailskas@ad.viko.lt>
2024-10-02 09:05:48 +0300

    value parameters

--- valueParametersExample/main.cpp
+++ valueParametersExample/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void functionValueParameters(int& num);
+void functionValueParameters(int num);
 int main()
 {
     int number = 6;
@@ -12,7 +12,7 @@ int main()
     return 0;
 }
 
-void functionValueParameters(int& num) {
+void functionValueParameters(int num) {
     cout<<"16 eilute. Funkcijai perduoto kintamojo reiksme num = "<<num<<endl;
     num = 20;
     cout<<"18 eilute. Funkcijoje pakeista num reiksme, kuri darbar lugi "<<num<<endl;
