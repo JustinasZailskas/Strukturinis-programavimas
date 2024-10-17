c6582369f56bd57ac63470bcef86931eef14d90e
JustinasZailskas <justinaszailskas@gmail.com>
2024-10-17 15:25:29 +0300

    Stulpelio elementu sumos atspausdinimas

--- Dvimatis-masyvas-pavyzdys/Dvimatis-masyvas/Dvimatis-masyvas.cpp
+++ Dvimatis-masyvas-pavyzdys/Dvimatis-masyvas/Dvimatis-masyvas.cpp
@@ -2,62 +2,56 @@
 #include <iomanip>
 using namespace std;
 const int EILUTES = 3;
-const int STULPELIAI = 2;
+const int STULPELIAI = 3;
 
 int main()
 {
-    
     int masyvas[EILUTES][STULPELIAI];
-
-    //Ivedimas 5x3 dydzio masyvo
-    cout << "Iveskite 5 eiluciu ir 3 stulpeliu masyvo elementus \n";
+    int determinantas = 0;
+    //Duomenu ivedimas 
+    cout << "Iveskite "<<EILUTES<<" ir "<<STULPELIAI<<" stulpeliu masyvo elementus \n";
     for (int i = 0; i < EILUTES; i++) {
         for (int j = 0; j < STULPELIAI; j++) {
             cout << "Masyvo [" << i + 1 << "][" << j + 1 << "] elementas \n";
             cin >> masyvas[i][j];
         }
     }
-    //Masyvo spausdinimas
-    cout << "Masyvo spausdinimas" << endl;
+    //Masyvo atspausdinimas
+    cout << "Masyvo atspausdinimas \n";
     for (int i = 0; i < EILUTES; i++) {
         for (int j = 0; j < STULPELIAI; j++) {
-            cout <<setw(5) <<masyvas[i][j]<<" ";
+            cout <<setw(5)<<masyvas[i][j];
         }
-        cout << endl;
-    }
-    cout << "Masyvo spausdinimas ir eilutes elementu suma" << endl;
-    for (int i = 0; i < EILUTES; i++) {
-        int eilutes_suma = 0;
-        for (int j = 0; j < STULPELIAI; j++) {
-            cout << setw(5) << masyvas[i][j] << " ";
-            eilutes_suma += masyvas[i][j];
-        }
-        cout << setw(5) << eilutes_suma;
         cout << endl;
     }
 
-    cout << "Masyvo spausdinimas ir stulpelio elementu suma" << endl;
+    cout << "Masyvo eilutes elementu sudetis \n";
+    for (int i = 0; i < EILUTES; i++) {
+        int eilutesSuma = 0;
+        for (int j = 0; j < STULPELIAI; j++) {
+            cout << setw(5) << masyvas[i][j];
+            eilutesSuma += masyvas[i][j];
+        }
+        cout<<setw(5) << eilutesSuma;
+        cout << endl;
+    }
+   
+    //Masyvo stulpelio elementu sudetis
+    int stulpeliuSumaMasyvas[STULPELIAI] = {0};
     for (int j = 0; j < STULPELIAI; j++) {
-        int stulpelio_suma = 0;
-        for (int i = 0; i < EILUTES
-            ; i++) {
-            cout << setw(5) << masyvas[i][j] << " ";
-            stulpelio_suma += masyvas[i][j];
+        int stulpelioSuma = 0;
+        for (int i = 0; i < EILUTES; i++) {
+            stulpelioSuma += masyvas[i][j];
         }
-
-        cout << endl;
-        cout<<setw(5) << "Stulpelio elementu suma " << stulpelio_suma;
+        stulpeliuSumaMasyvas[j] = stulpelioSuma;
     }
-
+    for (int i = 0; i < STULPELIAI; i++) {
+        cout << setw(5) << stulpeliuSumaMasyvas[i];
+    }
+    cout << endl;
+    determinantas = (masyvas[0][0] * masyvas[1][1] * masyvas[2][2]) + (masyvas[0][1] * masyvas[1][2] * masyvas[2][0]) + (masyvas[0][2] * masyvas[1][0] * masyvas[2][1]) - (masyvas[0][2] * masyvas[1][1] * masyvas[2][0]) - (masyvas[0][1] * masyvas[1][0] * masyvas[2][2]) - (masyvas[0][0] * masyvas[1][2] * masyvas[2][1]);
+    cout <<"Determinantas"<< setw(5) << determinantas;
 }
 
-//neveikia
-int gautiStulpelioElemSuma(int masyvas[][STULPELIAI], int eilutes, int stulpeliai) {
-    for (int j = 0; j < stulpeliai; j++) {
-        int stulpelio_suma = 0;
-        for (int i = 0; i < eilutes; i++) {
-            stulpelio_suma += masyvas[i][j];
-        }
-    }
-}
+
 
