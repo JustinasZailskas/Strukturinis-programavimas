fb9cd3230c54dc43a37ed1f32b7a15f7a84e4dfc
JustinasZailskas <justinaszailskas@gmail.com>
2024-11-06 09:51:20 +0200

    Nuskaito visa failo turini

--- pavyzdysSuFailais/main.cpp
+++ pavyzdysSuFailais/main.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 int main() {
     int masyvas[15];
+    int k = 0;
     ifstream failasSuSkaiciais;
     failasSuSkaiciais.open("skaiciai.txt");
-    for(int i=0; i < 10; i++) {
-        failasSuSkaiciais>>masyvas[i];
+    while(!failasSuSkaiciais.eof()) {
+        failasSuSkaiciais>>masyvas[k];
+        k++;
     }
     failasSuSkaiciais.close();
     cout<<endl;
-    for (int i = 0; i< 10; i++) {
+    for (int i = 0; i< 12; i++) {
         cout<<masyvas[i]<<" ";
     }
     return 0;
