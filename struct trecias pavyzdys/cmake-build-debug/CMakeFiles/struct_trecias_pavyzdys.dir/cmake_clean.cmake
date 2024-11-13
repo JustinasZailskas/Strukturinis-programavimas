59a29b182ce2934826db69bd52cdae74929c8ed9
JustinasZailskas <justinaszailskas@gmail.com>
2024-11-13 09:47:52 +0200

    Papildytas .txt failas reikiama informacija

---
+++ struct trecias pavyzdys/cmake-build-debug/CMakeFiles/struct_trecias_pavyzdys.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/struct_trecias_pavyzdys.dir/main.cpp.obj"
+  "CMakeFiles/struct_trecias_pavyzdys.dir/main.cpp.obj.d"
+  "libstruct_trecias_pavyzdys.dll.a"
+  "struct_trecias_pavyzdys.exe"
+  "struct_trecias_pavyzdys.exe.manifest"
+  "struct_trecias_pavyzdys.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/struct_trecias_pavyzdys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
